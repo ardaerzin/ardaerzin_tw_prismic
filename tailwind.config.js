001bbe662baa6099d38bcc8f04a9be274d9bffc6
Arda Erzin <ardaerzin@gmail.com>
2021-03-01 19:34:44 +0100

    theme updates
    - generated theme file now correctly exports the context
    - updates & improvements to the theme in general

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      gridTemplateColumns: {
+        'about-row': '1fr 10fr',
+        'about-col': '1fr'
+      },
       maxWidth: {
         'header-prose': '20ch',
         'ref-image': '12rem'
@@ -15,21 +18,12 @@ module.exports = {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        accent1: {
-          DEFAULT: '#261A3A'
-        },
-        accent2: {
-          DEFAULT: '#C18CC6'
-        },
-        accent3: {
-          DEFAULT: '#6843C5'
-        },
-        accent4: {
-          DEFAULT: '#75BEEF'
-        },
-        brand: {
-          DEFAULT: '#482584'
-        }
+        brand: 'var(--color-brand)',
+        accent1: 'var(--color-accent1)',
+        accent2: 'var(--color-accent2)',
+        accent3: 'var(--color-accent3)',
+        accent4: 'var(--color-accent4)',
+        bg: 'var(--color-bg)'
       }
     },
     fontFamily: {
